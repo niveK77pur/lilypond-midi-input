c9b1472e25d7a8fddfde31294d68cd93906a5fee
niveK77pur <kevinbiewesch@yahoo.fr>
2023-08-26 13:03:43 +0200

    feat: Monitor stdin and transfer input to lilypond thread
    Multi-threaded process, where one handles the MIDI inputs, and the other handles user input through stdin. The latter aims to allow changing of parameters on-the-fly without restarting the application.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,36 @@ use lilypond_midi_input as lmi;
 const BUFFER_SIZE: usize = 1024;
 
 fn main() {
-    // initialize the PortMidi context.
-    let context = portmidi::PortMidi::new().expect("At least one MIDI device available.");
-    let name = "USB-MIDI MIDI 1";
+    let (tx, rx) = std::sync::mpsc::channel();
 
-    lmi::list_devices(&context);
+    let lilypond_midi_input_handler = std::thread::spawn(move || {
+        // initialize the PortMidi context.
+        let context = portmidi::PortMidi::new().expect("At least one MIDI device available.");
+        let name = "USB-MIDI MIDI 1";
 
-    let port = lmi::MidiInputPort::new(name, &context, BUFFER_SIZE)
-        .expect("Port name matches an existing port");
+        lmi::list_devices(&context);
 
-    port.clear();
+        let port = lmi::MidiInputPort::new(name, &context, BUFFER_SIZE)
+            .expect("Port name matches an existing port");
 
-    port.listen(|event| println!("{:?}", lmi::MidiMessageType::from(event)))
-        .expect("Polling for new messages works.");
+        port.clear();
+
+        port.listen(|event| {
+            // TODO: buffer unreceived lines
+            let message = rx.try_recv();
+            println!("{:?} ({:?})", lmi::MidiMessageType::from(event), message)
+        }).expect("Polling for new messages works.");
+    });
+
+    let user_input_handler = std::thread::spawn(move || {
+        for line in std::io::stdin().lines() {
+            println!(">> GOT LINE: {:?}", line);
+            tx.send(line).expect("Receiver is alive");
+        }
+    });
+
+    match lilypond_midi_input_handler.join() {
+        Ok(_) => println!("Lilypond MIDI input handling thread finished."),
+        Err(e) => panic!("Lilypond MIDI input handling panicked: {:#?}", e),
+    };
 }
